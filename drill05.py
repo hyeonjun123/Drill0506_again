23c14e11bc950c09ca7a04397ecf2b317c970986
Im Hyeon Jun <hyeonjun3519@naver.com>
2023-10-04 22:02:20 +0900

    랜덤 위치 한개로 이동 완료, 방향에 대한 조정도 action 변수를 이용해서 처리완료

--- drill05.py
+++ drill05.py
@@ -29,25 +29,45 @@ def handle_events():
 def reset_world():
     global running, cx, cy, frame
     global hx, hy
+    global sx, sy
+    global t,action
+
+
     running = True
     cx, cy = TUK_WIDTH // 2, TUK_HEIGHT // 2
     frame = 0
+    action = 3
 
-    #hx, hy = TUK_WIDTH-50,TUK_HEIGHT-50
-    hx, hy = random.randint(0,TUK_WIDTH),random.randint(0,TUK_HEIGHT)
+
+    sx, sy = cx, cy # p1: 시작점
+    #hx, hy =50,50
+    hx, hy = random.randint(0,TUK_WIDTH), random.randint(0,TUK_HEIGHT) #p2: 끝점
+    t = 0.0
 
 
 def render_world():
     clear_canvas()
     TUK_ground.draw(TUK_WIDTH // 2, TUK_HEIGHT // 2)
     arrow.draw(hx,hy)
-    character.clip_draw(frame * 100, 100 * 1, 100, 100, cx, cy)
+    character.clip_draw(frame * 100, 100 * action, 100, 100, cx, cy)
     update_canvas()
 
 
 def update_world():
     global frame
+    global cx,cy
+    global t,action
+
     frame = (frame + 1) % 8
+
+    action = 1 if cx<hx else 0
+
+    if t<= 1.0:
+        t += 0.001
+        cx = (1-t)*sx + t*hx #cx는 시작점과 마지막점을 1-t : t비율로 섞음
+        cy = (1-t)*sy + t*hy
+
+
 
 
 open_canvas(TUK_WIDTH, TUK_HEIGHT)
